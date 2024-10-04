57d5958339828c63bcd40bca33ff2fcebaed82e1
zhangchenchengit <1520406629@qq.com>
2024-10-04 18:02:54 +0800

    边缘提取

---
+++ template_match_test.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+
+from common import cv_show
+
+img = cv.imread("img/liuyifei.jpeg")
+img_face = cv.imread("img/liuyifei_face.jpeg")
+
+h, w = img_face.shape[:2]
+print(h, w)
+
+res = cv.matchTemplate(img, img_face, cv.TM_SQDIFF)
+print(res.shape)
+
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+bottom_right = (min_loc[0] + w, min_loc[1] + h)
+
+img2 = img.copy()
+cv.rectangle(img2, min_loc, bottom_right, 255, 2)
+cv_show("img2", img2)
---
+++ template_multimatch_test.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import numpy as np
+
+from common import cv_show
+
+img = cv.imread("img/chongfu.jpeg")
+img_face = cv.imread("img/chongfu_unit.jpeg")
+
+h, w = img_face.shape[:2]
+print(h, w)
+
+# 模版可匹配多次
+res = cv.matchTemplate(img, img_face, cv.TM_CCOEFF_NORMED)
+print(res.shape)
+
+threshold = 0.8
+loc = np.where(res >= threshold)
+for pt in zip(*loc[::-1]):
+    bottom_right = pt[0] + w, pt[1] + h
+    cv.rectangle(img, pt, bottom_right, (0, 0, 255), 2)
+
+cv_show("img", img)
