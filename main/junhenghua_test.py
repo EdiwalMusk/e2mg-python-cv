3f70865ba53098ad83c2d23d036b969ab73b11b3
zhangchenchengit <1520406629@qq.com>
2024-10-04 20:33:04 +0800

    边缘提取

---
+++ main/junhenghua_test.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+
+from main.common import cv_show
+
+# 读取图像
+img = cv.imread("../img/junheng.png", 0)
+plt.hist(img.ravel(), 256)
+plt.show()
+
+equ = cv.equalizeHist(img)
+plt.hist(equ.ravel(), 256)
+plt.show()
+
+res = np.hstack((img, equ))
+cv_show('res', res)
+
+clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+res_clahe = clahe.apply(img)
+
+res = np.hstack((img, equ, res_clahe))
+cv_show("res", res)
